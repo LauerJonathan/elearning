d027b83abfc07b58419b0e9431f80077bd738f30
Jonathan.L <contact.pro@jonathanlauer.fr>
2024-11-23 22:10:28 +0100

    routing

---
+++ learn/src/pages/List/index.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../../components/NavBar";
+
+const List = () => {
+  return (
+    <>
+      <NavBar />
+      <h1>Hello worlds, page 2 !</h1>
+    </>
+  );
+};
+
+export default List;
