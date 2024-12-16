e7481a2e0cde011b92dd800d6485e3ed7f2aae6b
Jonathan.L <contact.pro@jonathanlauer.fr>
2024-12-16 12:34:35 +0100

    issue : favicon

--- learn/src/App.js
+++ learn/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       {/*basename="/hrnet/"*/}
-      <Routing basename="/hrnet/">
+      <Routing basename="">
         <UserContextProvider>
           <Routes>
             <Route exact path="/" element={<Home />}></Route>
